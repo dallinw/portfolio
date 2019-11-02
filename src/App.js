200adc3a89b4fe1fbb60f218809298b53a9e33de
dallinw <dallinjwilliams@gmail.com>
2019-11-02 16:02:55 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mikaella Antonio</h1>
+      <h3>I love you boosty!</h3>
+    </div>
+  );
+}
+
+export default App;
