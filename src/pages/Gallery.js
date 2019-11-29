8f3d786e5b6485b57c41510ced2a5a74a4bc6c8a
dallinw <dallinjwilliams@gmail.com>
2019-11-29 13:12:28 -0500

    styling fixes and code org

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -8,21 +8,20 @@ import './Gallery.css';
  * Constants
  *************************************************/
 
-// image objects, sorted by type
-const illustrations = [{title: 'Jame Our Bear', year: '2019', file: 'bear.png'},
-                       {title: 'Girls', year: '2019', file: 'girls.png'},
-                       {title: 'House', year: '2019', file: 'house.png'},
-                       {title: 'Memory Game', year: '2019', file: 'memory.png'},
-                       {title: 'Tongue', year: '2019', file: 'tongue.png'}]
-const paintings = [{title: 'Jame', year: '2019', file: 'jame.jpg'}];
-const prints = [{title: 'Restaurant', year: '2019', file: 'restaurant.png'},
-                {title: 'Houses Girls', year: '2019', file: 'housesgirls.png'}]
-const misc = [{title: 'Jumping', year: '2019', file: 'jumping.jpg'},
-              {title: 'Laughing', year: '2019', file: 'laughing.jpg'},
-              {title: 'Problem', year: '2019', file: 'problem.jpg'},
-              {title: 'Colorful Girls', year: '2019', file: 'colorfulgirls.jpg'}]
-const all = shuffle(illustrations.concat(paintings.concat(prints.concat(misc))));
-const fileNames = { 'illustrations': illustrations, 'paintings': paintings, 'prints': prints, 'all': all }
+// image objects
+const all = [{title: 'Restaurant', year: '2019', file: 'restaurant.png', type:'print'},
+                {title: 'Jame Our Bear', year: '2019', file: 'bear.png', type: 'illustration'},
+                {title: 'Girls', year: '2019', file: 'girls.png', type: 'illustration'},
+                {title: 'House', year: '2019', file: 'house.png', type: 'illustration'},
+                {title: 'Memory Game', year: '2019', file: 'memory.png', type: 'illustration'},
+                {title: 'Tongue', year: '2019', file: 'tongue.png', type: 'illustration'},
+                {title: 'Jame', year: '2019', file: 'jame.jpg', type: 'painting'},
+                {title: 'Houses Girls', year: '2019', file: 'housesgirls.png', type:'print'},
+                {title: 'Jumping', year: '2019', file: 'jumping.jpg'},
+                {title: 'Laughing', year: '2019', file: 'laughing.jpg'},
+                {title: 'Problem', year: '2019', file: 'problem.jpg'},
+                {title: 'Colorful Girls', year: '2019', file: 'colorfulgirls.jpg'}
+]
 const context = require.context('../assets/images', true);
 
 //classnames for responsiveness and show/hide
@@ -30,20 +29,22 @@ const BASE_CLASSNAME = "collectionButton";
 const BOLD_CLASSNAME = "collectionButton bold";
 const STD_BTN_CLASSNAME = "buttonBar";
 const MOB_BTN_CLASSNAME = "buttonBarMobile";
-const STD_IMG_CLASSNAME = "imgParent";
-const MOB_IMG_CLASSNAME = "imgParentMobile";
+const STD_IMG_PARENT_CLASSNAME = "imgParent";
+const MOB_IMG_PARENT_CLASSNAME = "imgParentMobile";
+const STD_IMG_CLASSNAME = "mainImg";
+const MOB_IMG_CLASSNAME = "mainImgMobile";
 const HIDE_BUTTON_CLASSNAME = "hiddenButton";
 const SHOW_BUTTON_CLASSNAME = "showButton";
 const NUM_COLUMNS = 3; // for the selection grid of images
-const buttonClasses = { all: BOLD_CLASSNAME, illustrations: BASE_CLASSNAME, paintings: BASE_CLASSNAME, prints: BASE_CLASSNAME }
+const buttonClasses = { all: BOLD_CLASSNAME, illustration: BASE_CLASSNAME, painting: BASE_CLASSNAME, print: BASE_CLASSNAME }
 
 const Gallery = () => {
   let [bigImgIndex, setBigImgIndex] = useState(0); // index to show the main image
   let [collection, setCollection] = useState("all"); // filtering collection selection
   let [showArrowsClassName, setShowArrowsClassName] = useState(HIDE_BUTTON_CLASSNAME); // show scroll arrows on hover
   const imageRef = useRef(null); // scroll to big image when a new image is selected
-  let collectionNames = fileNames[collection];
-  let images = getImages();
+  let imageData = filterImages();
+  let images = getContext();
 
   useEffect(() => {
     const handler = function(event) {
@@ -57,14 +58,24 @@ const Gallery = () => {
   })
 
   // get all the images from the collection for the grid
-  function getImages()
+  function filterImages()
   {
-    let paths = [];
-    collectionNames.forEach(name => {
+    if(collection === "all") return all;
+    var filtered = all.filter(function (obj) {
+      return obj.type === collection;
+    });
+    return filtered;
+  }
+
+  // from the filtered image array, get the actual files
+  function getContext()
+  {
+    let images = [];
+    imageData.forEach(name=> {
       let image = context(`./${name.file}`)
-      paths.push(image)
+      images.push(image);
     })
-    return paths;
+    return images;
   }
   
   // scroll forward or backward; triggered by click or by keypress
@@ -119,32 +130,34 @@ const Gallery = () => {
 
   // responsiveness
   let buttonBarClassName = STD_BTN_CLASSNAME;
-  let imgParentClassName = STD_IMG_CLASSNAME;
+  let imgParentClassName = STD_IMG_PARENT_CLASSNAME;
+  let imgClassName = STD_IMG_CLASSNAME;
   const isMobile = useMediaQuery({ query: '(max-width: 550px)' })
   if(isMobile)
   {
     buttonBarClassName=MOB_BTN_CLASSNAME;
-    imgParentClassName=MOB_IMG_CLASSNAME;
+    imgParentClassName=MOB_IMG_PARENT_CLASSNAME;
+    imgClassName=MOB_IMG_CLASSNAME;
   }
 
   return (
     <div className="gallery">
       <div className={buttonBarClassName}>
         <button className={buttonClasses["all"]} onClick={() => {changeCollection("all")}}>all</button>
-        <button className={buttonClasses["illustrations"]} onClick={() => {changeCollection("illustrations")}}>illustrations</button>
-        <button className={buttonClasses["paintings"]} onClick={() => {changeCollection("paintings")}}>paintings</button>
-        <button className={buttonClasses["prints"]} onClick={() => {changeCollection("prints")}}>prints</button>
+        <button className={buttonClasses["illustration"]} onClick={() => {changeCollection("illustration")}}>illustrations</button>
+        <button className={buttonClasses["painting"]} onClick={() => {changeCollection("painting")}}>paintings</button>
+        <button className={buttonClasses["print"]} onClick={() => {changeCollection("print")}}>prints</button>
       </div>
       <div className={imgParentClassName} onMouseEnter={()=>{toggleShowArrows()}} onMouseLeave={()=>{toggleShowArrows()}}>
       {(isMobile) ? null : 
         <div className={showArrowsClassName} onClick={() => {scrollImage("backward")}}>
           <button className="scrollButton"><img className="smallIcon" src={Back} alt="back"/></button>
         </div>}
-        <img onClick={() => {scrollImage("forward")}} ref={imageRef} className="mainImg" src={images[bigImgIndex]} alt=""/>
+        <img onClick={() => {scrollImage("forward")}} ref={imageRef} className={imgClassName} src={images[bigImgIndex]} alt=""/>
       <div className="scrollArea" onClick={() => {scrollImage("forward")}}>
         <div className="info">
-          <p className="title">{collectionNames[bigImgIndex].title}</p>
-          <p className="year">{collectionNames[bigImgIndex].year}</p>
+          <p className="title">{imageData[bigImgIndex].title}</p>
+          <p className="year">{imageData[bigImgIndex].year}</p>
         </div>
         {(isMobile) ? null: <div className={showArrowsClassName}>
           <button className="scrollButton"><img className="smallIcon" src={Forward} alt="forward"/></button>
@@ -158,18 +171,6 @@ const Gallery = () => {
         </div>
     </div>
   )
-}
-
-/**
- * Shuffles array in place
- * @param {Array} a items An array containing the items.
- */
-function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
 }
 
 export default Gallery;
