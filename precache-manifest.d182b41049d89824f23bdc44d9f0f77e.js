7bd4415109366a595d3a6c58702ecd61add9acef
dallinw <dallinjwilliams@gmail.com>
2019-11-30 12:15:37 -0500

    Updates

---
+++ precache-manifest.d182b41049d89824f23bdc44d9f0f77e.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63099e7b38e917d1f10f7fe2453019bc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4ac6d6a09b79e8870f72",
+    "url": "/portfolio/static/css/main.03be0b2e.chunk.css"
+  },
+  {
+    "revision": "2643f53f1e2904592312",
+    "url": "/portfolio/static/js/2.b720f9be.chunk.js"
+  },
+  {
+    "revision": "4ac6d6a09b79e8870f72",
+    "url": "/portfolio/static/js/main.5e14f5ab.chunk.js"
+  },
+  {
+    "revision": "37e3e34e9e87bc734bcf",
+    "url": "/portfolio/static/js/runtime-main.45c83192.js"
+  },
+  {
+    "revision": "8dd1d6a873c7d66a9f61e62fa4f7fb42",
+    "url": "/portfolio/static/media/bear.8dd1d6a8.png"
+  },
+  {
+    "revision": "e9ac59a63fe9599f5b751108112ff8a6",
+    "url": "/portfolio/static/media/bear.e9ac59a6.pdf"
+  },
+  {
+    "revision": "9a4acd67acd07a9de8f2672199b221ed",
+    "url": "/portfolio/static/media/colorfulgirls.9a4acd67.jpg"
+  },
+  {
+    "revision": "c21d5b95827d037b4daf9817241cb7f4",
+    "url": "/portfolio/static/media/girls.c21d5b95.pdf"
+  },
+  {
+    "revision": "c5c60b92a192a9e951b01f7b9d8b5190",
+    "url": "/portfolio/static/media/girls.c5c60b92.png"
+  },
+  {
+    "revision": "3356ee720ea2be1e713e0c6e7583c792",
+    "url": "/portfolio/static/media/house.3356ee72.png"
+  },
+  {
+    "revision": "7d07db6c00c8a78e0c37356ee0d2eaf2",
+    "url": "/portfolio/static/media/house.7d07db6c.pdf"
+  },
+  {
+    "revision": "802add75aa07b5a88fc88905e8bbec85",
+    "url": "/portfolio/static/media/housesgirls.802add75.png"
+  },
+  {
+    "revision": "482fdafbad3cef179c17f0d5974b942d",
+    "url": "/portfolio/static/media/jame.482fdafb.jpg"
+  },
+  {
+    "revision": "89236c1029720fccca3546a2d8d2aa8c",
+    "url": "/portfolio/static/media/jumping.89236c10.jpg"
+  },
+  {
+    "revision": "f1fee573455d4878bd2ab3da82b138c9",
+    "url": "/portfolio/static/media/laughing.f1fee573.jpg"
+  },
+  {
+    "revision": "38c4d1cc5a125fd166bdc5f8ccb0f063",
+    "url": "/portfolio/static/media/memory.38c4d1cc.png"
+  },
+  {
+    "revision": "f52e2025c8e6a7e8bd73ae784095dda3",
+    "url": "/portfolio/static/media/memory.f52e2025.pdf"
+  },
+  {
+    "revision": "8d6ae45d178b52c1762d6f67a452f14e",
+    "url": "/portfolio/static/media/problem.8d6ae45d.jpg"
+  },
+  {
+    "revision": "3fc0c56c56f9eabef71fff4d5a4e21dc",
+    "url": "/portfolio/static/media/puzzle.3fc0c56c.pdf"
+  },
+  {
+    "revision": "d6c3c5218624851ead6aab3b3b9d4121",
+    "url": "/portfolio/static/media/puzzle1.d6c3c521.pdf"
+  },
+  {
+    "revision": "d6c3c5218624851ead6aab3b3b9d4121",
+    "url": "/portfolio/static/media/puzzle2.d6c3c521.pdf"
+  },
+  {
+    "revision": "3fc0c56c56f9eabef71fff4d5a4e21dc",
+    "url": "/portfolio/static/media/puzzle3.3fc0c56c.pdf"
+  },
+  {
+    "revision": "3fc0c56c56f9eabef71fff4d5a4e21dc",
+    "url": "/portfolio/static/media/puzzle4.3fc0c56c.pdf"
+  },
+  {
+    "revision": "3fc0c56c56f9eabef71fff4d5a4e21dc",
+    "url": "/portfolio/static/media/puzzle5.3fc0c56c.pdf"
+  },
+  {
+    "revision": "7dd9680b5fc866a6acfe83046b28dddd",
+    "url": "/portfolio/static/media/restaurant.7dd9680b.png"
+  },
+  {
+    "revision": "1aff30afee7b7c0b46ac3cf94582081d",
+    "url": "/portfolio/static/media/search.1aff30af.png"
+  },
+  {
+    "revision": "69194ea5d926431ba98f91ef01176749",
+    "url": "/portfolio/static/media/search1.69194ea5.pdf"
+  },
+  {
+    "revision": "37b56e76b5538f8655f53fec0fb965b3",
+    "url": "/portfolio/static/media/search2.37b56e76.pdf"
+  },
+  {
+    "revision": "cfcf50a636ba77c57e058ad749cec3bc",
+    "url": "/portfolio/static/media/search3.cfcf50a6.pdf"
+  },
+  {
+    "revision": "23182849abcaa0fb89b2d06c0696b86a",
+    "url": "/portfolio/static/media/tongue.23182849.png"
+  },
+  {
+    "revision": "746fced1b94ab4da60d1b1f2ae66b193",
+    "url": "/portfolio/static/media/tongue.746fced1.pdf"
+  }
+]);
