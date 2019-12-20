48393d753dcfc3b1ec1ca6b01dcd55899739ea9d
williamsdj6 <dallin.j.williams@leidos.com>
2019-12-20 09:10:21 -0500

    Updates

---
+++ precache-manifest.35a75ea818b3b31ae4e3f0bf05e4ef4f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577d2040df54be8b0b06fdb202fbde38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2301cf37ebd5c7cbc056",
+    "url": "/static/css/main.268176a2.chunk.css"
+  },
+  {
+    "revision": "e423975b99ab8454994a",
+    "url": "/static/js/2.6a93a3de.chunk.js"
+  },
+  {
+    "revision": "4460da46d566383d3650",
+    "url": "/static/js/3.58d87aee.chunk.js"
+  },
+  {
+    "revision": "2301cf37ebd5c7cbc056",
+    "url": "/static/js/main.c5079303.chunk.js"
+  },
+  {
+    "revision": "e5fe39dcc946e83dd0bd",
+    "url": "/static/js/runtime-main.d6981be0.js"
+  },
+  {
+    "revision": "e9ac59a63fe9599f5b751108112ff8a6",
+    "url": "/static/media/bear.e9ac59a6.pdf"
+  },
+  {
+    "revision": "9a4acd67acd07a9de8f2672199b221ed",
+    "url": "/static/media/colorfulgirls.9a4acd67.jpg"
+  },
+  {
+    "revision": "784b0ebcfad3778118472d6b0be3b125",
+    "url": "/static/media/destroyer search.784b0ebc.pdf"
+  },
+  {
+    "revision": "cb7837b4f584f4587ebacc29d56d5fb8",
+    "url": "/static/media/falling girls search.cb7837b4.pdf"
+  },
+  {
+    "revision": "1022e4f3b988043513b73e247d007e95",
+    "url": "/static/media/fancy search.1022e4f3.pdf"
+  },
+  {
+    "revision": "53dc9ccc9c4df2d810c83df8c9b97d62",
+    "url": "/static/media/fight search.53dc9ccc.pdf"
+  },
+  {
+    "revision": "3356ee720ea2be1e713e0c6e7583c792",
+    "url": "/static/media/house.3356ee72.png"
+  },
+  {
+    "revision": "802add75aa07b5a88fc88905e8bbec85",
+    "url": "/static/media/housesgirls.802add75.png"
+  },
+  {
+    "revision": "89236c1029720fccca3546a2d8d2aa8c",
+    "url": "/static/media/jumping.89236c10.jpg"
+  },
+  {
+    "revision": "f1fee573455d4878bd2ab3da82b138c9",
+    "url": "/static/media/laughing.f1fee573.jpg"
+  },
+  {
+    "revision": "8d6ae45d178b52c1762d6f67a452f14e",
+    "url": "/static/media/problem.8d6ae45d.jpg"
+  },
+  {
+    "revision": "7dd9680b5fc866a6acfe83046b28dddd",
+    "url": "/static/media/restaurant.7dd9680b.png"
+  },
+  {
+    "revision": "705d05d15ba8429874d5ace0fb1e9bf9",
+    "url": "/static/media/search.705d05d1.png"
+  },
+  {
+    "revision": "06bc23f3f52b9be2e6f0675d69848e68",
+    "url": "/static/media/toy search.06bc23f3.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0b36509d3d835ccf375a27d68559f09.js"
+  "/precache-manifest.35a75ea818b3b31ae4e3f0bf05e4ef4f.js"
 );
 
 self.addEventListener('message', (event) => {
